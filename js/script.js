a09721b8bc422b2f9d92c7789f931ad145b5134f
HaVanDuoc <94791378+HaVanDuoc@users.noreply.github.com>
2022-04-02 15:29:04 +0700

    Edit lại file js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-// "use strict";
+"use strict";
 
-window.onload = (
+window.onload = () => { menuToggle(); }
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop()) {
-            $(".hvd__navbar").addClass("sticky");
-        } else {
-            $(".hvd__navbar").removeClass("sticky");
-        }
-    }),
+window.onscroll = () => { scrollFunction(); }
 
+// Function Toggle For Button Menu
+const menuToggle = () => {
+    const menu = document.getElementById('menu');
+    const menuBtn = document.getElementById('menu-btn');
 
-    menuToggle = () => {
-        const menu = document.getElementById('menu');
-        const menuBtn = document.getElementById('menu-btn');
+    menuBtn.addEventListener('mouseenter', () => {
+        menu.classList.add('active');
+        menuBtn.classList.add('active');
+    })
+    window.addEventListener('click', () => {
+        menu.classList.remove('active');
+        menuBtn.classList.remove('active');
+    })
+};
 
-        menuBtn.addEventListener('mouseenter', () => {
-            menu.classList.add('active');
-            menuBtn.classList.add('active');
-        })
-        window.addEventListener('click', () => {
-            menu.classList.remove('active');
-            menuBtn.classList.remove('active');
-        })
+// Function Scroll For Sticky Navbar
+const scrollFunction = () => {
+
+    var navbar = document.querySelector(".hvd__navbar");
+    var sticky = navbar.offsetHeight;
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
     }
-);
+};
 
 
